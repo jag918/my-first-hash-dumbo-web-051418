9972e4bc5af7023b93de1912094e33ef2bb30571
jee <jag918@nyu.edu>
2018-05-08 04:03:09 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
-  my_hash = {"Jee"=>"Graeff"}
+  my_hash = {"marvel"=>"Spiderman"}
 end
 
 
